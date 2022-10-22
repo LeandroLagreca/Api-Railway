58eeeb31c00de1f4bcd4eff8456a43fc8493affa
leandro la greca <leandrolagreca@hotmail.com>
2022-10-22 02:11:34 -0300

    p

--- env.js
+++ env.js
@@ -1,4 +1,4 @@
-postgres= postgres
+DB_USER = postgres
 DB_PASSWORD= 1234
 DB_HOST= localhost
 DB_PORT = 3000
--- src/db.js
+++ src/db.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const path = require('path');
 const { generateKey } = require('crypto');
 const {
-  postgres, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 // cambiar name de db
-const sequelize = new Sequelize(`postgres://${postgres}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   // CONFIGURCION ADICIONAL
