8abb8e0c98e5e165045ab0f67d7ce06e4cd5f941
leandro la greca <leandrolagreca@hotmail.com>
2022-10-22 01:46:13 -0300

    db port

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-
+const {DB_PORT} = require('procces.env');
 require('./db.js');
 const cors = require('cors');
 
@@ -12,14 +12,14 @@ const server = express();
 server.name = 'API';
 
 //Hecho por nosotros :S
-server.use(cors({ origin: 'http://localhost:3000' }));
+server.use(cors({ origin: DB_PORT }));
 //aca termina jajas
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', DB_PORT); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
