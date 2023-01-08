3f0a5d69e958a91dcd66c988d8b8a8280cede6f7
leandro la greca <leandrolagreca@hotmail.com>
2023-01-07 21:28:48 -0300

    port deploy

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { Game } = require('./src/loadGamesDB/loadGame.js')
 const { genresToDb } = require('./src/loadGamesDB/loadGenre.js')
-const port= process.env.PORT || 3001
+const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   genresToDb().then(()=>{
     Game().then(()=>{
       server.listen(port, () => {
-          console.log(`%s listening at ${port}`); // eslint-disable-line no-console
+         // console.log(`%s listening at ${port}`); // eslint-disable-line no-console
       });
     });
   });
