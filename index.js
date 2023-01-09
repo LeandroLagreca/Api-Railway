8492490e95d64c5259b1a9605789d5e691079fee
leandro la greca <leandrolagreca@hotmail.com>
2023-01-09 00:20:55 -0300

    deploy2

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { Game } = require('./src/loadGamesDB/loadGame.js')
 const { genresToDb } = require('./src/loadGamesDB/loadGenre.js')
-const port = process.env.PORT || 3001
+require('dotenv').config();
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   genresToDb().then(()=>{
     Game().then(()=>{
-      server.listen(port, () => {
+      server.listen(process.env.PORT, () => {
          console.log('%s listening at ${port}'); // eslint-disable-line no-console
       });
     });
